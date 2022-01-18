20c76b17fc72526a68a1bdc3ad15991eb4d90884
Ajay Kumar <ajaykumarmkaju@gmail.com>
2022-01-19 00:37:30 +0530

    Updated models and homepage

--- message/models.py
+++ message/models.py
@@ -3,5 +3,7 @@ from django.db import models
 
 class Message(models.Model):
     recieverName = models.CharField(max_length = 20, blank = False)
-    messageToSend = models.CharField(max_length = 120, blank = False)
-     
+    messageToSend = models.TextField(max_length = 120, blank = False)
+    
+    def __str__(self):
+        return (self.recieverName,self.messageToSend)
