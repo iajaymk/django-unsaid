ac575c84eda18f5e2aae3673668a7b12fa00d7fb
Ajay Kumar <ajaykumarmkaju@gmail.com>
2022-01-18 22:33:18 +0530

    Added Message Model

--- message/models.py
+++ message/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    recieverName = models.CharField(max_length = 20, blank = False)
+    messageToSend = models.CharField(max_length = 120, blank = False)
+     
