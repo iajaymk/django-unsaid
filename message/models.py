c95dc2c9c5af4e571fab85d112639abed3de9df4
Ajay Kumar <ajaykumarmkaju@gmail.com>
2022-01-22 14:26:20 +0530

    Error

--- message/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Message(models.Model):
-    recieverName = models.CharField(max_length = 20, blank = False)
-    messageToSend = models.TextField(max_length = 120, blank = False)
-    
-    def __str__(self):
-        return (self.recieverName,self.messageToSend)
