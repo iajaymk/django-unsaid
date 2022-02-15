bb271f90915161e614eb89e06d7eb724e7c6b7e5
Ajay Kumar <ajaykumarmkaju@gmail.com>
2022-02-15 16:56:50 +0530

    Added messages.html

--- message/views.py
+++ message/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'message/index.html')
+
+
+def submit(request):
+    return render(request, 'message/message.html')
