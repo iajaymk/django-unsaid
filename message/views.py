f189f28a193f05650cef430ca9fcac681593dd53
Ajay Kumar <ajaykumarmkaju@gmail.com>
2022-02-17 14:11:58 +0530

    modified

--- message/views.py
+++ message/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from . models import Message
+
+from django.views.generic import (ListView,CreateView,DetailView)
+
 
 def home(request):
-    return render(request,'message/index.html')
+    messages = Message.objects.all()
+    return render(request,'message/index.html',{'messages':messages})
 
 
 def submit(request):
-    return render(request, 'message/message.html')
+    if request.method == "POST":
+        print('done')
+
+
+        
+        return render(request,'message/index.html')
+    else:
+        return render(request, 'message/message.html')
+            
